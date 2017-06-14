88bf4e315f62f2b5592b11535ccd26876a21c1ce
Simon Tucker <srtucker22@gmail.com>
2017-06-14 09:51:52 +0500

    Step 11.6: Update Resolvers and Logic

--- server/data/resolvers.js
+++ server/data/resolvers.js
@@ -46,6 +46,9 @@ export const Resolvers = {
     updateGroup(_, args, ctx) {
       return groupLogic.updateGroup(_, args, ctx);
     },
+    updateUser(_, args, ctx) {
+      return userLogic.updateUser(_, args, ctx);
+    },
     login(_, signinUserInput, ctx) {
       // find user by email
       const { email, password } = signinUserInput.user;
@@ -165,6 +168,9 @@ export const Resolvers = {
     },
     messages(user, args, ctx) {
       return userLogic.messages(user, args, ctx);
+    },
+    registrationId(user, args, ctx) {
+      return userLogic.registrationId(user, args, ctx);
     },
   },
 };
